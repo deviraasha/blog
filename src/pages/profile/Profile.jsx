90381dfd9d6e7befbad31acc0860a8777cd1f493
deviraasha <deviraasha@gmail.com>
2022-01-10 15:23:05 +0700

    first

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import "./profile.css"
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile() {
+    return (
+        <>
+            <Topbar />
+            <div className="profile">
+                <Sidebar />
+                <div className="profileRight">
+                    <div className="profileRiightTop">
+                        <div className="profileCover">
+                            <img src="/assets/post/4.jpeg" alt="" className="profileCoverImg" />
+                            <img src="/assets/person/1.jpg" alt="" className="profileUserImg" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">
+                                Asha Cantik
+                            </h4>
+                            <span className="profileInfoDesc">Hallo this is asha hehe</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <Rightbar profile />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
